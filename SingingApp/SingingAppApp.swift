614bbdb491de53d9b468c17e4eeb7bb50e2cb503
Koutarou Arima <ok220058@oks.iput.ac.jp>
2025-07-01 19:39:39 +0900

    Initial Commit

---
+++ SingingApp/SingingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SingingAppApp.swift
+//  SingingApp
+//
+//  Created by Koutarou Arima on 2025/07/01.
+//
+
+import SwiftUI
+
+@main
+struct SingingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
